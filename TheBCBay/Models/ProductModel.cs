f540c9d8d19a536b2070bd897a9dd8d7a8320689
clauber_rox <cklauber@gmail.com>
2018-02-05 17:23:15 -0700

    Created the Produc Model

---
+++ TheBCBay/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBCBay.Models
+{
+    public class ProductModel
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal CurrentPrice { get; set; }
+        public decimal TopPrice { get; set; }
+        public decimal LowPrice { get; set; }
+        public int UsrId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndTime { get; set; }
+        //Add Photos
+
+    }
+}
