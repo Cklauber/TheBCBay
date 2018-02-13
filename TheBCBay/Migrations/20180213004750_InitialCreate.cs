87b5600f4807c9950cdd301cfaaff10ef593e03e
clauber_rox <cklauber@gmail.com>
2018-02-12 18:43:06 -0700

    Added a database to store the items listed.

---
+++ TheBCBay/Migrations/20180213004750_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TheBCBay.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CurrentPrice = table.Column<decimal>(nullable: false),
+                    Description = table.Column<string>(maxLength: 1000, nullable: false),
+                    EndTime = table.Column<DateTime>(nullable: false),
+                    LowPrice = table.Column<decimal>(nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    Title = table.Column<string>(maxLength: 25, nullable: false),
+                    TopPrice = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Items");
+        }
+    }
+}
---
+++ TheBCBay/Services/SqlItemData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBCBay.Data;
+using TheBCBay.Models;
+
+namespace TheBCBay.Services
+{
+    public class SqlItemData : IItemData
+    {
+        private TheBCBayDBContext _context;
+        public SqlItemData(TheBCBayDBContext context)
+        {
+            _context = context;
+        }
+
+        public ItemModel Add(ItemModel newItem)
+        {
+            _context.Items.Add(newItem);
+            _context.SaveChanges();
+            return newItem;
+        }
+
+        public ItemModel Get(int id)
+        {
+            return _context.Items.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<ItemModel> GetAll()
+        {
+            return _context.Items.OrderBy(r => r.Title);
+        }
+    }
+}
